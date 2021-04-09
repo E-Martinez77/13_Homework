c1d7e17efd9596ee1de4f65ddfe3da431a842034
E-Martinez77 <emartizzle2185@me.com>
2021-04-08 20:05:41 -0600

    updates

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INTEGER AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(30) NOT NULL,
-    devoured BOOLEAN NOT NULL DEFAULT false
+    devoured BOOLEAN NOT NULL DEFAULT false,
     PRIMARY KEY (id)
 );
