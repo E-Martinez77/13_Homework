185718ed9e570c0ed0a7373513f57705ab974c1f
E-Martinez77 <emartizzle2185@me.com>
2021-04-11 20:54:43 -0600

    full teardown

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -1,40 +1,36 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-const burger = require("../models/burger");
+const cat = require('../models/cat.js');
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", (req, res) => {
-  burger.all((data) => {
+router.get('/', (req, res) => {
+  cat.all((data) => {
     const hbsObject = {
-      burger: data,
+      cats: data,
     };
     console.log(hbsObject);
-    res.render("index", hbsObject);
+    res.render('index', hbsObject);
   });
 });
 
-router.post("/api/burger", (req, res) => {
-  burger.create(
-    ["name", "devour"],
-    [req.body.name, req.body.devour],
-    (result) => {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    }
-  );
+router.post('/api/cats', (req, res) => {
+  cat.create(['name', 'sleepy'], [req.body.name, req.body.sleepy], (result) => {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
 });
 
-router.put("/api/burger/:id", (req, res) => {
+router.put('/api/cats/:id', (req, res) => {
   const condition = `id = ${req.params.id}`;
 
-  console.log("condition", condition);
+  console.log('condition', condition);
 
-  burger.update(
+  cat.update(
     {
-      sleepy: req.body.devour,
+      sleepy: req.body.sleepy,
     },
     condition,
     (result) => {
@@ -47,10 +43,10 @@ router.put("/api/burger/:id", (req, res) => {
   );
 });
 
-router.delete("/api/burger/:id", (req, res) => {
+router.delete('/api/cats/:id', (req, res) => {
   const condition = `id = ${req.params.id}`;
 
-  burger.delete(condition, (result) => {
+  cat.delete(condition, (result) => {
     if (result.affectedRows === 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
--- models/cat.js
+++ models/cat.js
@@ -1,21 +1,21 @@
 // Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
 
-const burger = {
+const cat = {
   all(cb) {
-    orm.all("burgers", (res) => cb(res));
+    orm.all("cats", (res) => cb(res));
   },
   // The variables cols and vals are arrays.
   create(cols, vals, cb) {
-    orm.create("burgers", cols, vals, (res) => cb(res));
+    orm.create("cats", cols, vals, (res) => cb(res));
   },
   update(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, (res) => cb(res));
+    orm.update("cats", objColVals, condition, (res) => cb(res));
   },
   delete(condition, cb) {
-    orm.delete("burgers", condition, (res) => cb(res));
+    orm.delete("cats", condition, (res) => cb(res));
   },
 };
 
 // Export the database functions for the controller (catsController.js).
-module.exports = burger;
+module.exports = cat;
